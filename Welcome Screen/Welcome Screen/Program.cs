f5d55782c3e554895836eba6a84a916b560103a0
Rex <wreckseal@yahoo.com>
2026-06-30 16:53:37 -0600

    Writing and Running Code in C# Assignment

---
+++ Welcome Screen/Welcome Screen/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string message = "Welcome to Acme Accounting Systems\nRemember, we're \"accounting\" on you!";
+        Console.WriteLine(message);
+    }
+}
