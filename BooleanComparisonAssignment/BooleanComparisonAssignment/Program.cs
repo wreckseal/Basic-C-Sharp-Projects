36c4821ef6918accb723b3c3bd48b1503a50bae4
Rex <wreckseal@yahoo.com>
2026-07-03 17:53:08 -0600

    Boolean comparison using a while and do-while statement.

---
+++ BooleanComparisonAssignment/BooleanComparisonAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BooleanComparisonAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0; // Initialize a counter variable
+            Console.WriteLine("While Loop:"); // Print a message indicating the start of the while loop
+            while (counter < 10) // Loop until counter is less than 10
+            {
+                counter++; // Increment the counter variable
+                Console.WriteLine($"counter: {counter}"); // Print the current value of counter
+            }
+
+            Console.WriteLine("\nDo-While Loop:"); // Print a message indicating the start of the do-while loop
+            do // Start a do-while loop
+            {
+                Console.WriteLine($"counter: {counter}"); // Print the current value of counter. No condition is checked before the first iteration of the loop, so this will always execute at least once.
+                counter--; // Decrement the counter variable
+            }
+            while (counter > 0); // Continue looping while counter is greater than 0
+        }
+    }   
+}
